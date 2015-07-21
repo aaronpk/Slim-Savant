29d0161ea6bfdf09469b0588b48a586341666089
Aaron Parecki <aaron@parecki.com>
2015-07-20 18:29:34 -0700

    add dependency

--- SlimSavant.php
+++ SlimSavant.php
@@ -2,12 +2,37 @@
 namespace Slim\Savant;
 
 function init() {
+  global $app;
+
   // Configure the Savant plugin
   \Slim\Extras\Views\Savant::$savantDirectory = 'vendor/saltybeagle/savant3';
   \Slim\Extras\Views\Savant::$savantOptions = array('template_path' => 'views');
 
   // Create a new app object with the Savant view renderer
-  return new \Slim\Slim(array(
+  $app = new \Slim\Slim(array(
     'view' => new \Slim\Extras\Views\Savant()
   ));
+}
+
+function render($page, $data=array()) {
+  global $app;
+  return $app->render('layout.php', array_merge($data, array('page' => $page)));
+};
+
+function partial($template, $data=array(), $debug=false) {
+  global $app;
+
+  if($debug) {
+    $tpl = new Savant3(\Slim\Extras\Views\Savant::$savantOptions);
+    echo '<pre>' . $tpl->fetch($template . '.php') . '</pre>';
+    return '';
+  }
+
+  ob_start();
+  $tpl = new Savant3(\Slim\Extras\Views\Savant::$savantOptions);
+  foreach($data as $k=>$v) {
+    $tpl->{$k} = $v;
+  }
+  $tpl->display($template . '.php');
+  return ob_get_clean();
 }
